72d5908fb5e4e9c272e5450a10562e32c95cd481
Lucyellen <luh130893@gmail.com>
2017-09-14 18:16:52 -0300

    correcoes

--- Public/js/config.js
+++ Public/js/config.js
@@ -3,8 +3,10 @@ function setConfig()
     var texts = {
         "title" : "Controle de compras"
     };
+
     document.title = texts.title;
-    document.getElementById("navTitle").innerHTML = texts.title;
+    document.getElementById("valores").innerHTML = texts.title;
+
 }
 
 setConfig();
